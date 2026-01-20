affd8315a60aade7a1f43f4943dd1a49af75cb55
Kieran Barry <pa20830@bristol.ac.uk>
2026-01-20 11:35:26 +0000

    againb

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class BuildWithCMake(build_py):
         subprocess.check_call(["cmake", "--build", "."], cwd=build_dir)
 
         # 2. Copy executables into the Python package
-        output_bin_dir = os.path.join("gfoil", "bin")
+        output_bin_dir = os.path.join("GFoil", "bin")
         os.makedirs(output_bin_dir, exist_ok=True)
         shutil.copy(os.path.join(build_dir, "GFoil_fwd_codi"), output_bin_dir)
         shutil.copy(os.path.join(build_dir, "GFoil_AD"), output_bin_dir)
